29a7a47f702eedf602a9976fad76a84d646a777c
Cindy Nilsson <dante@nilsson.name>
2024-10-03 11:59:35 +0200

    Adding onedarkpro

--- lua/custom/plugins/color.lua
+++ lua/custom/plugins/color.lua
@@ -1,21 +1,20 @@
 return {
 	{
-		"kvrohit/rasmus.nvim",
+		"olimorris/onedarkpro.nvim",
 		config = function()
-			vim.g.rasmus_italic_comments = false
-			vim.g.rasmus_bold_comments = false
-			vim.g.rasmus_italic_keywords = true
-			vim.g.rasmus_bold_keywords = false
-			vim.g.rasmus_italic_booleans = false
-			vim.g.rasmus_bold_booleans = false
-			vim.g.rasmus_italic_functions = false
-			vim.g.rasmus_bold_functions = true
-			vim.g.rasmus_italic_variables = false
-			vim.g.rasmus_bold_variables = false
+			--vim.g.rasmus_italic_comments = false
+			--vim.g.rasmus_bold_comments = false
+			--vim.g.rasmus_italic_keywords = true
+			--vim.g.rasmus_bold_keywords = false
+			--vim.g.rasmus_italic_booleans = false
+			--vim.g.rasmus_bold_booleans = false
+			--vim.g.rasmus_italic_functions = false
+			--vim.g.rasmus_bold_functions = true
+			--vim.g.rasmus_italic_variables = false
+			--vim.g.rasmus_bold_variables = false
 
-			vim.g.rasmus_variant = "dark"
 
-			vim.cmd.colorscheme("rasmus")
+			vim.cmd.colorscheme("onedarkpro")
 		end,
 	},
 	{
